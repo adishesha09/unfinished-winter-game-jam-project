1a3ef35f6812c7b86f78172ee494f1dfcc478f1e
Adishesha Sai Nandkoomar <ST10470336@vgconnect.edu.za>
2026-03-25 14:16:09 +0200

    Add input system, player scripts and IDE files
    Add autogenerated Unity InputSystem_Actions (input mappings) and its meta, introduce PlayerController and PlayerCameraController scripts with metas, and add/modify related project assets. Also include .idea IDE config files, SceneTemplateSettings.json, and updates to SampleScene.unity, Mobile_RPAsset and TagManager.asset to wire up the new input/player setup.

---
+++ Assets/Scripts/Player/PlayerCameraController.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [SerializeField] private Transform followTarget;
+    [SerializeField] private Vector3 cameraOffset = new Vector3(0f, 2f, -10f);
+    [SerializeField] private float fixedPitch = 8f;
+
+    [SerializeField] private float horizontalSmoothTime = 0.12f;
+    [SerializeField] private float verticalRiseSmoothTime = 0.25f;
+    [SerializeField] private float verticalFallSmoothTime = 0.08f;
+
+    [SerializeField] private float lookAheadDistance = 2.5f;
+    [SerializeField] private float lookAheadSmoothTime = 0.4f;
+
+    [SerializeField] private float verticalDeadzone = 0.8f;
+
+    [SerializeField] private float baseFov = 62f;
+    [SerializeField] private float sprintFovBonus = 8f;
+    [SerializeField] private float fallFovBonus = 5f;
+    [SerializeField] private float fallFovMaxSpeed = 15f;
+    [SerializeField] private float fovSmoothTime = 0.25f;
+    [SerializeField] private float fovRampStartSpeed = 6f;
+    [SerializeField] private float fovRampEndSpeed = 9f;
+
+    [SerializeField] private float maxShakeAngle = 3f;
+    [SerializeField] private float traumaDecayRate = 1.5f;
+
+    [SerializeField] private bool useLevelBounds;
+    [SerializeField] private Vector2 boundsMin = new Vector2(-50f, -10f);
+    [SerializeField] private Vector2 boundsMax = new Vector2(50f, 20f);
+
+    [SerializeField] private bool useCameraHeightProgression = true;
+    [SerializeField] private float levelStartX = -50f;
+    [SerializeField] private float levelEndX = 50f;
+    [SerializeField] private float progressionStartYDelta = -1f;
+    [SerializeField] private float progressionEndYDelta = 2f;
+
+    private Camera _camera;
+
+    private float _xVelocity;
+    private float _yVelocity;
+    private float _fovVelocity;
+    private float _lookAheadX;
+    private float _lookAheadVelocity;
+    private float _anchoredY;
+    private float _trauma;
+    private float _currentHorizontalSpeed;
+    private float _currentVerticalSpeed;
+
+    private Vector3 _previousTargetPosition;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    private void Start()
+    {
+        if (followTarget != null)
+            SnapToTarget();
+
+        if (_camera != null)
+            _camera.fieldOfView = baseFov;
+    }
+
+    private void LateUpdate()
+    {
+        if (followTarget == null) return;
+
+        _currentHorizontalSpeed = (followTarget.position.x - _previousTargetPosition.x) / Time.deltaTime;
+        _currentVerticalSpeed = (followTarget.position.y - _previousTargetPosition.y) / Time.deltaTime;
+
+        UpdateLookAhead();
+        UpdateVerticalAnchor();
+        UpdatePosition();
+        UpdateFov();
+        ApplyScreenShake();
+
+        _previousTargetPosition = followTarget.position;
+    }
+
+    public void AddTrauma(float amount)
+    {
+        _trauma = Mathf.Clamp01(_trauma + amount);
+    }
+
+    public void SnapToTarget()
+    {
+        if (followTarget == null) return;
+
+        float x = followTarget.position.x + cameraOffset.x;
+        float y = followTarget.position.y + DynamicYOffset;
+        float z = followTarget.position.z + cameraOffset.z;
+
+        transform.position = new Vector3(x, y, z);
+        _anchoredY = y;
+        _xVelocity = 0f;
+        _yVelocity = 0f;
+        _lookAheadX = 0f;
+        _lookAheadVelocity = 0f;
+        _previousTargetPosition = followTarget.position;
+
+        if (_camera != null)
+            _camera.fieldOfView = baseFov;
+    }
+
+    private void UpdateLookAhead()
+    {
+        float speedFraction = Mathf.Clamp(_currentHorizontalSpeed / fovRampEndSpeed, -1f, 1f);
+        float targetLookAhead = speedFraction * lookAheadDistance;
+        _lookAheadX = Mathf.SmoothDamp(_lookAheadX, targetLookAhead, ref _lookAheadVelocity, lookAheadSmoothTime);
+    }
+
+    private void UpdateVerticalAnchor()
+    {
+        float playerTargetY = followTarget.position.y + DynamicYOffset;
+
+        if (playerTargetY > _anchoredY + verticalDeadzone)
+            _anchoredY = playerTargetY - verticalDeadzone;
+        else if (playerTargetY < _anchoredY - verticalDeadzone)
+            _anchoredY = playerTargetY + verticalDeadzone;
+    }
+
+    private void UpdatePosition()
+    {
+        float targetX = followTarget.position.x + cameraOffset.x + _lookAheadX;
+        float targetY = _anchoredY;
+        float targetZ = followTarget.position.z + cameraOffset.z;
+
+        float smoothedX = Mathf.SmoothDamp(transform.position.x, targetX, ref _xVelocity, horizontalSmoothTime);
+
+        float verticalSmoothTime = targetY < transform.position.y ? verticalFallSmoothTime : verticalRiseSmoothTime;
+        float smoothedY = Mathf.SmoothDamp(transform.position.y, targetY, ref _yVelocity, verticalSmoothTime);
+
+        if (useLevelBounds)
+        {
+            smoothedX = Mathf.Clamp(smoothedX, boundsMin.x, boundsMax.x);
+            smoothedY = Mathf.Clamp(smoothedY, boundsMin.y, boundsMax.y);
+        }
+
+        transform.position = new Vector3(smoothedX, smoothedY, targetZ);
+    }
+
+    private void UpdateFov()
+    {
+        if (_camera == null) return;
+
+        float sprintProgress = Mathf.SmoothStep(0f, 1f, Mathf.InverseLerp(fovRampStartSpeed, fovRampEndSpeed, Mathf.Abs(_currentHorizontalSpeed)));
+        float fallProgress = Mathf.SmoothStep(0f, 1f, Mathf.InverseLerp(0f, -fallFovMaxSpeed, _currentVerticalSpeed));
+
+        float targetFov = baseFov + sprintProgress * sprintFovBonus + fallProgress * fallFovBonus;
+        _camera.fieldOfView = Mathf.SmoothDamp(_camera.fieldOfView, targetFov, ref _fovVelocity, fovSmoothTime);
+    }
+
+    private void ApplyScreenShake()
+    {
+        _trauma = Mathf.Max(0f, _trauma - traumaDecayRate * Time.deltaTime);
+        float shake = _trauma * _trauma;
+
+        float shakeX = maxShakeAngle * shake * (Mathf.PerlinNoise(Time.time * 40f, 0f) * 2f - 1f);
+        float shakeY = maxShakeAngle * shake * (Mathf.PerlinNoise(0f, Time.time * 40f) * 2f - 1f);
+
+        transform.rotation = Quaternion.Euler(fixedPitch + shakeY, shakeX, 0f);
+    }
+
+    private float DynamicYOffset
+    {
+        get
+        {
+            if (!useCameraHeightProgression || followTarget == null)
+                return cameraOffset.y;
+
+            float progress = Mathf.SmoothStep(0f, 1f, Mathf.InverseLerp(levelStartX, levelEndX, followTarget.position.x));
+            return cameraOffset.y + Mathf.Lerp(progressionStartYDelta, progressionEndYDelta, progress);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,173 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour, InputSystem_Actions.IPlayerActions
+{
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float sprintSpeed = 9f;
+    [SerializeField] private float acceleration = 15f;
+    [SerializeField] private float deceleration = 20f;
+    [SerializeField] private float rotationSpeed = 540f;
+
+    [SerializeField] private float jumpHeight = 6f;
+    [SerializeField] private float gravityMultiplier = 1.8f;
+    [SerializeField] private float fallGravityMultiplier = 3f;
+    [SerializeField] private float minFallMultiplier = 1f;
+    [SerializeField] private float fallGravityRampSpeed = 8f;
+    [SerializeField] private float jumpCutMultiplier = 0.5f;
+    [SerializeField] private float apexHangThreshold = 2f;
+    [SerializeField] private float apexGravityScale = 0.5f;
+    [SerializeField] private float terminalFallSpeed = 20f;
+    [SerializeField] private float coyoteTime = 0.15f;
+    [SerializeField] private float jumpBufferDuration = 0.2f;
+
+    private CharacterController _characterController;
+    private InputSystem_Actions _inputActions;
+
+    private Vector2 _rawMoveInput;
+    private Vector3 _horizontalVelocity;
+    private float _verticalVelocity;
+
+    private bool _sprintHeld;
+    private bool _jumpBuffered;
+    private bool _isJumping;
+
+    private float _coyoteTimeCounter;
+    private float _jumpBufferCounter;
+
+    private float EffectiveGravity => Physics.gravity.y * gravityMultiplier;
+    private float TargetSpeed => _sprintHeld ? sprintSpeed : walkSpeed;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _inputActions = new InputSystem_Actions();
+        _inputActions.Player.SetCallbacks(this);
+    }
+
+    private void OnEnable() => _inputActions.Player.Enable();
+
+    private void OnDisable() => _inputActions.Player.Disable();
+
+    private void OnDestroy() => _inputActions.Dispose();
+
+    private void Update()
+    {
+        UpdateGroundState();
+        TickJumpBuffer();
+        ProcessJump();
+        ProcessHorizontalMovement();
+        ApplyGravity();
+
+        _characterController.Move((_horizontalVelocity + Vector3.up * _verticalVelocity) * Time.deltaTime);
+    }
+
+    private void UpdateGroundState()
+    {
+        if (_characterController.isGrounded)
+        {
+            _coyoteTimeCounter = coyoteTime;
+            _isJumping = false;
+            if (_verticalVelocity < 0f)
+                _verticalVelocity = -2f;
+        }
+        else
+        {
+            _coyoteTimeCounter -= Time.deltaTime;
+        }
+    }
+
+    private void TickJumpBuffer()
+    {
+        if (_jumpBuffered)
+        {
+            _jumpBufferCounter = jumpBufferDuration;
+            _jumpBuffered = false;
+        }
+        else
+        {
+            _jumpBufferCounter -= Time.deltaTime;
+        }
+    }
+
+    private void ProcessJump()
+    {
+        if (_jumpBufferCounter > 0f && _coyoteTimeCounter > 0f)
+        {
+            _verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * EffectiveGravity);
+            _coyoteTimeCounter = 0f;
+            _jumpBufferCounter = 0f;
+            _isJumping = true;
+        }
+    }
+
+    private void ProcessHorizontalMovement()
+    {
+        Vector3 desiredDirection = GetMoveDirection();
+        Vector3 targetVelocity = desiredDirection * TargetSpeed;
+        float blendRate = desiredDirection.sqrMagnitude > 0f ? acceleration : deceleration;
+
+        _horizontalVelocity = Vector3.MoveTowards(_horizontalVelocity, targetVelocity, blendRate * Time.deltaTime);
+
+        if (Mathf.Abs(_horizontalVelocity.x) > 0.01f)
+        {
+            float targetYaw = _horizontalVelocity.x > 0f ? 90f : -90f;
+            Quaternion targetRotation = Quaternion.Euler(0f, targetYaw, 0f);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private Vector3 GetMoveDirection()
+    {
+        if (Mathf.Abs(_rawMoveInput.x) < 0.01f)
+            return Vector3.zero;
+
+        return new Vector3(_rawMoveInput.x, 0f, 0f).normalized;
+    }
+
+    private void ApplyGravity()
+    {
+        float multiplier;
+
+        if (_isJumping && Mathf.Abs(_verticalVelocity) <= apexHangThreshold)
+        {
+            float t = Mathf.SmoothStep(0f, 1f, 1f - (Mathf.Abs(_verticalVelocity) / apexHangThreshold));
+            multiplier = Mathf.Lerp(1f, apexGravityScale, t);
+        }
+        else if (_verticalVelocity < 0f)
+        {
+            float fallProgress = Mathf.SmoothStep(0f, 1f, Mathf.InverseLerp(0f, fallGravityRampSpeed, -_verticalVelocity));
+            multiplier = Mathf.Lerp(minFallMultiplier, fallGravityMultiplier, fallProgress);
+        }
+        else
+        {
+            multiplier = 1f;
+        }
+
+        _verticalVelocity += EffectiveGravity * multiplier * Time.deltaTime;
+        _verticalVelocity = Mathf.Max(_verticalVelocity, -terminalFallSpeed);
+    }
+
+    public void OnMove(InputAction.CallbackContext context) =>
+        _rawMoveInput = context.ReadValue<Vector2>();
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+            _jumpBuffered = true;
+
+        if (context.canceled && _isJumping && _verticalVelocity > 0f)
+            _verticalVelocity *= jumpCutMultiplier;
+    }
+
+    public void OnSprint(InputAction.CallbackContext context) =>
+        _sprintHeld = context.ReadValueAsButton();
+
+    public void OnLook(InputAction.CallbackContext context) { }
+    public void OnAttack(InputAction.CallbackContext context) { }
+    public void OnInteract(InputAction.CallbackContext context) { }
+    public void OnCrouch(InputAction.CallbackContext context) { }
+    public void OnPrevious(InputAction.CallbackContext context) { }
+    public void OnNext(InputAction.CallbackContext context) { }
+}
